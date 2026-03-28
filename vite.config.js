320557b8a124fee0ffbc49696b82f972a3d245ee
hemanthkrishna27 <35033882+hemanthkrishna27@users.noreply.github.com>
2026-03-28 12:25:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/DocsAI/',  // Add this line for GitHub Pages
+  base: '/docsai-frontend/',  // Add this line for GitHub Pages
   plugins: [react()],
   server: {
     port: 3000,
